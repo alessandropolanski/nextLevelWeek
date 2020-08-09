64cd4658c3f632b628e794796966d3cbe621f113
alessandropolanski <alessandropolanski91@gmail.com>
2020-08-09 18:26:32 -0300

    Criação de Paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Landing from './pages/Landing/index';
+import Routes from './routes';
+
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <Landing/> 
+    <Routes/> 
   );
 }
 
